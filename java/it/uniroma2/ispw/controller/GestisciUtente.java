30427b423a821735feeda35deff0d183e9d858a4
jesus9 <andrealeo09@gmail.com>
2016-12-27 12:58:02 +0100

    trybranch

--- java/it/uniroma2/ispw/controller/GestisciUtente.java
+++ java/it/uniroma2/ispw/controller/GestisciUtente.java
@@ -20,6 +20,7 @@ public class GestisciUtente {
 	
 	public boolean effettuaRegistrazione(String nome, String cognome, String email, String password, 
 			String telefono, String residenza, String type){
+		int a = 5;
 		if(u.checkUtente(email, password) == true){
 			return false;
 		}
