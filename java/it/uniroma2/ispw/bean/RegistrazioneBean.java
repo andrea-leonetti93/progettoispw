7c6d950978a5886fe44954b41a9c47e77e27032a
giorgio <giorgio@giorgio-SATELLITE-L850-1HU>
2016-12-28 13:22:19 +0100

    fff

--- java/it/uniroma2/ispw/bean/RegistrazioneBean.java
+++ java/it/uniroma2/ispw/bean/RegistrazioneBean.java
@@ -5,6 +5,7 @@ import it.uniroma2.ispw.model.UtenteRegistrato;
 
 public class RegistrazioneBean {
 
+	private String prova;
 	private String name;
     private String surname;
     private String email;
